0f7140f6fe226c291aac979af462c9170bc462f1
eliyahumoshkoviz <054epm@gmail.com>
2024-07-24 16:47:50 +0300

    separated into sub code

--- index.js
+++ index.js
@@ -1,62 +1,29 @@
 const fs = require("fs");
-const { trashMessage } = require("./functions");
+const { parseFileToArr, createMessageObj } = require("./functions");
+
+
 
 function parseFileContent(filePath) {
-  const fileContent = fs.readFileSync(filePath, "utf8");
-  const regex = /(\d{1,2}\/\d{1,2}\/\d{2,4}, \d{2}:\d{2}) - /g;
-  const lines = fileContent.split(regex).filter(Boolean);
-  // const arr = [];
+  const lines = parseFileToArr(filePath);
+  const arr = [];
 
-  // for (let i = 0; i < lines.length; i++) {
+  for (let i = 0; i < lines.length; i += 2) {
+    const date = lines[i];
+    const messageContent = lines[i + 1];
+    const result = createMessageObj(date, messageContent)
 
-  //   if (i % 2 === 0) {
-  //     const date = arrRef[i];
-  //     const messageContent = arrRef[i + 1];
-  //     const regex = /([^:]+):\s*/;
-  //     const match = messageContent.match(regex);
-
-  //   }
-
-  //   if (match) {
-  //     const sender = match[1];
-  //     const message = messageContent.slice(match[0].length);
-
-  //     if (trashMessage(message)) break;
-
-  //     const isQuestion = sender !== 'הרב ברוך אפרתי, (טאטע)';
-
-  //     arr.push({ date, sender, message, isQuestion });
-
-  //   }
-
-
-  // }
-
-  return lines.reduce((arr, _, i, arrRef) => {
-    if (i % 2 === 0) {
-      const date = arrRef[i];
-      const messageContent = arrRef[i + 1];
-      const regex = /([^:]+):\s*/;
-      const match = messageContent.match(regex);
-      if (match) {
-        const sender = match[1];
-        const message = messageContent.slice(match[0].length);
-debugger
-        if (trashMessage(message)) {
-          return arr;
-        }
-
-        const isQuestion = sender !== 'הרב ברוך אפרתי, (טאטע)';
-        arr.push({ date, sender, message, isQuestion });
-      }
+    if (result){
+      arr.push(result);
     }
-    return arr;
-  }, []);
+  }
+  return arr
+
 }
+
 
 const result = parseFileContent("test.txt");
 
-console.log(result);
+console.log(result[0].messages);
 
 
 
