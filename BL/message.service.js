0dc98010afcfaff18234dce96435487a85c03ae3
eliyahumoshkoviz <054epm@gmail.com>
2024-07-25 14:27:27 +0300

    uploud mongo

--- BL/message.service.js
+++ BL/message.service.js
@@ -2,11 +2,8 @@ import { create, read, readOne } from "../DL/controllers/message.controller.js";
 import { parseFileContent } from "../functions.js";
 
 
-export const createMessageService = (fileContent) => {
-    return parseFileContent(fileContent);
-
-
-    //  create(message)
+export const createMessageService = async (fileContent) => {
+    return await create(parseFileContent(fileContent))
 };
 
 export const readMessagesService = (filter) => read(filter);
