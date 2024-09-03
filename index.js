c3f234371b3b50a363492c425833ce2b040294f6
meir vitkin <vitkin4580@email.com>
2024-09-03 12:47:58 +0300

    [org ]

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 
 import { connect } from './DL/db.js';
 import messageRoutes from './routes/message.route.js';
+import editedQaRoutes from './routes/editedQa.route.js';
 
 
 const app = express();
@@ -12,6 +13,8 @@ connect();
 app.use(cors());
 app.use(express.json());
 app.use('/messages', messageRoutes);
+app.use('/qa', editedQaRoutes);
+
 const port = process.env.PORT || 5050;
 
 app.listen(port, () => console.log(`#### Server running on port ${port} ####`));
